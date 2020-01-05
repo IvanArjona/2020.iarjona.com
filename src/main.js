4a72ef725ebae7c869d59bf8c07ce8d5c2c565b7
Iván <ivanyn9@gmail.com>
2020-01-05 14:03:15 +0100

    Import modernizr

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedinIn, faGithub, faGitlab, faFacebook, faTelegramPlane } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import Modernizr from './vendor/modernizr-custom';
 
-const Modernizr = require('./vendor/modernizr-custom');
 Modernizr.webp;
 
 library.add(faLinkedinIn, faGithub, faGitlab, faFacebook, faTelegramPlane, faEnvelope);
