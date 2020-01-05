9212bf1e7b0ba8e52f3e21c7550d3c5a21ac878a
Iván <ivanyn9@gmail.com>
2020-01-05 17:09:34 +0100

    Modernizr as script

--- src/main.js
+++ src/main.js
@@ -5,9 +5,6 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedinIn, faGithub, faGitlab, faFacebook, faTelegramPlane } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import Modernizr from '../vendor/modernizr-custom';
-
-Modernizr.webp;
 
 library.add(faLinkedinIn, faGithub, faGitlab, faFacebook, faTelegramPlane, faEnvelope);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
